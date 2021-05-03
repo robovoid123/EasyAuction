6878cbfc672e0a9f205ea4c91d25d475dffe31f2
void <void@pop-os.localdomain>
2021-05-03 20:18:36 +0545

    Setting up DB

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,7 @@ import secrets
 from typing import Any, Dict, List, Optional, Union
 
 from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from databases import DatabaseURL
 
 
 class Settings(BaseSettings):
@@ -27,20 +28,14 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
-    SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
+    POSTGRES_PORT: str
+    SQLALCHEMY_DATABASE_URI: Optional[DatabaseURL]
 
     @validator("SQLALCHEMY_DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
         if isinstance(v, str):
             return v
-        return PostgresDsn.build(
-            scheme="postgresql",
-            user=values.get("POSTGRES_USER"),
-            password=values.get("POSTGRES_PASSWORD"),
-            host=values.get("POSTGRES_SERVER"),
-            port=values.get("POSTGRES_PORT"),
-            path=f"/{values.get('POSTGRES_DB') or ''}",
-        )
+        return DatabaseURL(url=f"postgresql://{values.get('POSTGRES_USER')}:{values.get('POSTGRES_PASSWORD')}@{values.get('POSTGRES_SERVER')}:{values.get('POSTGRES_PORT')}/{values.get('POSTGRES_DB')}")
 
     class Config:
         case_sensitive = True
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import router
 from app.core.config import settings
+from app.core import tasks
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
@@ -17,6 +18,9 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+app.add_event_handler("startup", tasks.create_start_app_handler(app))
+app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
 
 
 app.include_router(router, prefix=settings.API_V1_STR)
