1b9abd3076a01292eb89f2999559b66c841aa611
void <void@pop-os.localdomain>
2021-05-23 09:41:57 +0545

    .

--- backend/app/v1/auction/bid.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-from app.easy_auction.base import Base
-from app.crud.auction.bid import crud_bid
-
-
-class Bid(Base):
-    def __init__(self, db: Session):
-        super().__init__(crud_bid, db)
