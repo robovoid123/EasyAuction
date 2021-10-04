745ce8cf0b9e7c3fce73ff3dad54a1c59f40247c
void <void@pop-os.localdomain>
2021-10-04 11:04:49 +0545

    get bidder bid auctions

--- backend/app/modules/auction/api/v1/bids.py
+++ backend/app/modules/auction/api/v1/bids.py
@@ -1,3 +1,23 @@
-from fastapi import APIRouter
+
+from typing import Dict, List, Tuple
+from app.modules.auction.schemas.auction import AuctionInDB
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app.dependencies.database import get_db
+
+from app.modules.auction.repositories.bid import bid_repo
 
 router = APIRouter()
+
+
+@router.get("/{bidder_id}/auctions", response_model=List[Tuple[AuctionInDB, Dict]])
+def get_bidder_bid_auctions(*,
+                            bidder_id: int,
+                            skip: int = 0,
+                            limit: int = 5,
+                            db: Session = Depends(get_db)
+                            ):
+    api_resp = bid_repo.get_bidder_auction(
+        db, skip=skip, limit=limit, bidder_id=bidder_id)
+    return api_resp
--- backend/app/modules/auction/repositories/bid.py
+++ backend/app/modules/auction/repositories/bid.py
@@ -1,11 +1,23 @@
+from sqlalchemy.orm.session import Session
+from sqlalchemy import func, desc
 from app.modules.auction.schemas import BidCreate, BidUpdate
 from app.modules.auction.models import Bid
 from app.repository.repository_base import BaseRepository
 
+from app.modules.auction.models.auction import Auction
+
 
 class BidRepository(BaseRepository[Bid, BidCreate,
                                    BidUpdate]):
-    pass
+    def get_bidder_auction(self, db: Session, *, skip: int = 0, limit: int = 100, bidder_id: int):
+        bid_count = func.count(self.model.auction_id)
+        auctions = db.query(self.model.auction_id, bid_count.label("bidder_bid_count")).join(Auction, self.model.auction_id == Auction.id).filter(
+            self.model.bidder_id == bidder_id).group_by(self.model.auction_id)
+        auctions = auctions.order_by(
+            desc(bid_count)).offset(skip).limit(limit).all()
+        auctions = [(db.query(Auction).get(a.auction_id), {
+                     'bidder_bid_count': a.bidder_bid_count}) for a in auctions]
+        return auctions
 
 
 bid_repo = BidRepository(Bid)
